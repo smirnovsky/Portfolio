ce7b8c0d5c697ba7a329b9d8ce38756817cddb3f
smirnovsky <happy280190@gmail.com>
2023-05-17 20:06:02 +0300

    start to implement Header and Projects sections

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle>
+        Welcome To <br /> My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpouse of javascriptmastery Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aliquid?
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'} >Learn more</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,36 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id='projects' >
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {[
+        {
+          title: 'Project1',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
+        },
+        {
+          title: 'Project2',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
+        },
+        {
+          title: 'Project3',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
+        },
+        {
+          title: 'Project4',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
+        },
+      ].map((project) => (
+        <div>
+          {project.title}
+          <br />
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
