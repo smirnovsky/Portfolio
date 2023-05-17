991c538a1f82f7780efbd14e590d45bb994900f6
smirnovsky <happy280190@gmail.com>
2023-05-17 20:46:18 +0300

    finish Header and Projects sections

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,29 +9,27 @@ const Projects = () => (
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {[
-        {
-          title: 'Project1',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
-        },
-        {
-          title: 'Project2',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
-        },
-        {
-          title: 'Project3',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
-        },
-        {
-          title: 'Project4',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eius illum temporibus unde non error!'
-        },
-      ].map((project) => (
-        <div>
-          {project.title}
-          <br />
-          {project.description}
-        </div>
+      {projects.map(({ id, image, title, description, tags, source, visit }) => (
+        <BlogCard key={id}>
+          <Img src={image}/>
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i} >{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
       ))}
     </GridContainer>
   </Section>
